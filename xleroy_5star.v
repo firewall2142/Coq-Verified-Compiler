fa72dfeb8d361906de7f55502ee0a57607a93fc4
Ashish <ashbrnwl.2000@gmail.com>
2019-12-20 21:03:37 +0530

    update

--- xleroy_5star.v
+++ xleroy_5star.v
@@ -104,11 +104,6 @@ Inductive instruction: Type :=
 Definition code := list instruction.
 Definition stack := list nat.
 Definition configuration := (nat * stack)%type.
-Definition varmap := id -> option nat. 
-
-Definition empty_varmap : varmap := fun _ => None.
-Definition update_varmap (f : varmap) (varname : id) (abspos : nat) : varmap :=
-  fun t:id => if beq_id t varname then Some abspos else (f t).
 
 Fixpoint code_at (C:code) (pc: nat) : option instruction :=
   match C, pc with
@@ -134,7 +129,6 @@ Fixpoint set_nth_slot (s:stack) (n:nat) (v:nat): option stack :=
     | Some ns' => Some (u :: ns')
     end
   end.
-
 
 
 Inductive transition (C: code): configuration -> configuration -> Prop :=
@@ -184,130 +178,56 @@ Inductive transition (C: code): configuration -> configuration -> Prop :=
                                           transition C (pc, n2 :: n1 :: stk) (pc', stk).
 
 
-Fixpoint compile_aexp (a: aexp) (vmp : varmap) (stk_size : nat): code :=
+Definition varlist := list string.
+
+Fixpoint find (var : string) (vlist : varlist) : option nat :=
+  match vlist with
+  | nil => None
+  | u::vlist' => if beq_id (Id u) (Id var) then
+               Some O
+             else
+               match find var vlist' with
+               | None => None
+               | Some n => Some (S n)
+               end
+  end.
+
+
+
+Fixpoint gen_vlist (c:com) (ivlist: varlist): varlist :=
+  match c with
+  | c1 ;; c2 => let v1 := gen_vlist c1 ivlist in
+                gen_vlist c2 v1
+  | IFB b THEN c1 ELSE c2 FI => let v1 := gen_vlist c1 ivlist in
+                                gen_vlist c2 v1
+  | WHILE b DO c1 END => gen_vlist c1 ivlist
+  | CAss x a => match find x ivlist with
+                | None => x::ivlist
+                | _ => nil
+                end
+  | CSkip => nil
+  end.
+
+Definition get_vlist (c:com): varlist :=
+  gen_vlist c nil.
+    
+Definition X := ("X"%string).
+Definition Y := ("Y"%string).
+Definition Z := ("Z"%string).
+
+
+Definition mycode :=
+  (
+     X ::= ANum 3 ;; WHILE BTrue DO Y ::= AId (Id X) END
+  ).
+
+Compute (find Z (get_vlist mycode)).
+
+Fixpoint compile_aexp (a:aexp) (stklen: nat) (vlist : varlist): (code * nat) :=
   match a with
   | ANum n => Iconst n :: nil
-  | AId x =>  match vmp x with
-              | None => Iconst 0 :: nil
-              | Some n => Iget (stk_size - n - 1) :: nil
-              end
-  | APlus a1 a2 =>  (compile_aexp a1 vmp stk_size) ++ (compile_aexp a2 vmp (stk_size+1)) ++ (Iadd :: nil)
-  | AMinus a1 a2 => (compile_aexp a1 vmp stk_size) ++ (compile_aexp a2 vmp (stk_size+1)) ++ (Isub :: nil)
-  | AMul a1 a2 =>   (compile_aexp a1 vmp stk_size) ++ (compile_aexp a2 vmp (stk_size+1)) ++ (Imul :: nil)
-  end.
-
-Fixpoint compile_bexp (b: bexp) (vmp: varmap) (stk_size : nat) (cond:bool) (ofs: nat): code :=
-  match b with
-  | BTrue =>        if cond then (Ibranch_forward ofs :: nil) else nil
-  | BFalse =>       if cond then nil else (Ibranch_forward ofs :: nil)
-  | BEq a1 a2 =>    (compile_aexp a1 vmp stk_size) ++ (compile_aexp a2 vmp (stk_size+1)) ++
-                    (if cond then (Ibeq ofs)::nil else (Ibne ofs)::nil)
-  | BLe a1 a2 =>    (compile_aexp a1 vmp stk_size) ++ (compile_aexp a2 vmp (stk_size+1)) ++
-                    (if cond then (Ible ofs)::nil else (Ibgt ofs)::nil)
-  | BNot b1 =>      compile_bexp b1 vmp stk_size (negb cond) ofs
-  | BAnd b1 b2 =>   let c2 := (compile_bexp b2 vmp (stk_size+1) cond ofs) in
-                    let ofsc1:nat := if cond then (length c2) else (ofs + length c2) in
-                    let c1 := (compile_bexp b1 vmp stk_size false ofsc1) in
-                    (c1 ++ c2)
-  end.
-
-Definition test_vmap :=
-  fun t:id => if beq_id t (Id "X") then Some 0
-              else if beq_id t (Id "Y") then Some 1
-              else None.
-
-Definition X := AId (Id "X").
-Definition Y := AId (Id "Y").
-Definition Z := AId (Id "Z").
-Definition W := AId (Id "W").
-
-(* X + (X + Y)*)
-Definition testexp1 := (APlus (X) (APlus (X) (Y)) ).
-Compute (compile_aexp testexp1 test_vmap 2).
-
-(* Y + (X + Y)*)
-Definition testexp2 := (APlus (Y) (APlus (X) (Y)) ).
-Compute (compile_aexp testexp2 test_vmap 2).
-
-
-Definition test_vmap3 := 
-  fun t:id => if beq_id t (Id "Z") then Some 2 else test_vmap t.
-
-(* Z + (2 + Y)*)
-Definition testexp3 := (APlus (Z) (APlus (X) (Y)) ).
-Compute (compile_aexp testexp3 test_vmap3 3).
-
-(* (X*X - 2*X*Y) + Y*)
-Definition testexp4 := (APlus (AMinus (AMul X X) (AMul (ANum 2) (AMul X Y))) Y ).
-Compute (  compile_aexp testexp4 test_vmap 2  ).
-
-
-(* compile com * init_stack * varmap -> code * stack * varmap*)
-Fixpoint compile_com (c : com) (stack_len : nat)(v : varmap): (code * (nat * varmap)) :=
-  match c with
-  | SKIP                             => (nil , (stack_len, v))
-  | c1 ;; c2                         => let comp1 := (compile_com c1 stack_len v) in
-                                        let comp2 := (compile_com c2 (fst (snd comp1)) (snd (snd comp1)) ) in
-                                        (((fst comp1) ++ (fst comp2)), snd comp2)
-  | x ::= a                          => match v (Id x) with
-                                        | None =>  (    compile_aexp a v (stack_len)   ,
-                                                      ( stack_len + 1 , update_varmap v (Id x) stack_len )      )
-                                        | Some pos => (   (compile_aexp a v (stack_len)) ++ (Iset (stack_len-(pos+1))::nil ),
-                                                      ( stack_len, v)  )
-                                        end
-                                        
-  | IFB b THEN ifso ELSE ifnotso FI  => let code_ifso := (compile_com ifso stack_len v) in
-                                        let code_ifnotso := (compile_com ifnotso stack_len v) in
-                                        let ifelse_code := (compile_bexp b v stack_len false ((length (fst code_ifso)) + 1))
-                                          ++ (fst code_ifso)
-                                          ++ (Ibranch_forward (length (fst code_ifnotso)) :: nil)
-                                          ++ (fst code_ifnotso)
-                                        in
-                                        (ifelse_code, ())
-  | WHILE b DO body END              => let code_body := (compile_com body stack_len v) in
-                                        let code_test := compile_bexp b false (length code_body + 1) in
-                                        code_test
-                                          ++ code_body
-                                          ++ Ibranch_backward (length code_test + length code_body + 1) :: nil
-  end.                                          
-
-                                             
-                                                 
-                                                            
-                                                            
-                                             
+  | AId x => match find vlist x with
+             | None => Iconst 0
+             | Some n => Iget (stklen-1+n)
+                              
   
-                                  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
